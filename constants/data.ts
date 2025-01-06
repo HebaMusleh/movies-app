c0c9940baca88846487cd193a784d39861064b63
hebamesluh1 <hebamesluh@gmail.com>
2025-01-06 15:38:17 +0200

    add mock data

--- constants/data.ts
+++ constants/data.ts
@@ -1,3 +1,15 @@
+import m1 from "../assets/images/movie-1.png";
+import m2 from "../assets/images/movie-2.png";
+import m3 from "../assets/images/movie-3.png";
+import m4 from "../assets/images/movie-4.png";
+import m5 from "../assets/images/movie-5.png";
+import m6 from "../assets/images/movie-6.png";
+import m7 from "../assets/images/movie-7.png";
+import m8 from "../assets/images/movie-8.png";
+import m9 from "../assets/images/movie-9.png";
+import m10 from "../assets/images/movie-10.png";
+
+import header from "../assets/images/header.png";
 
 export const mockMovieData = [
   {
@@ -8,8 +20,116 @@ export const mockMovieData = [
     actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
     description:
       "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
-    image: "",
-    header: "",
+    image: m1,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 2,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m2,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 3,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m3,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 4,
+    title: "The Shawshank Redemption",
+    year: 1924,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m4,
+    header: header,
+    reviews: 7.5,
+  },
+  {
+    id: 5,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 120,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m5,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 6,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m6,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 7,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m7,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 7,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m8,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 7,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m9,
+    header: header,
+    reviews: 9.5,
+  },
+  {
+    id: 7,
+    title: "The Shawshank Redemption",
+    year: 1904,
+    runtime: 170,
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+    description:
+      "From DC Comics comes the Suicide Squad, an antihero team of incarcerated supervillains who act as deniable assets for the United States government, undertaking high-risk black ops missions in exchange for commuted prison sentences.",
+    image: m10,
+    header: header,
     reviews: 9.5,
   },
 ];
---
+++ types/types.ts
@@ -0,0 +1,11 @@
+export interface IData {
+  id: number;
+  title: string;
+  year: number;
+  runtime: number;
+  actors: string;
+  description: string;
+  image: any;
+  header: any;
+  reviews: number;
+}
