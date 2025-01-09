14ea0fc933e9023d5073a4d64fa91db6976d22b5
hebamesluh1 <hebamesluh@gmail.com>
2025-01-09 12:24:25 +0200

    all screen ui done

--- components/WatchListComponents/Card/Card.tsx
+++ components/WatchListComponents/Card/Card.tsx
@@ -1,48 +1,75 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
-type Props = {}
-
-const Card = (props: Props) => {
+const Card = () => {
   return (
     <View style={styles.wrapper}>
       <View>
         <Image
-            style={styles.ImageStyle}
-            source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-         />
+          style={styles.ImageStyle}
+          source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+        />
       </View>
-      <View>
+      <View style={{ justifyContent: "space-evenly" }}>
         <Text style={styles.titleStyle}>Movie Title</Text>
         <View>
-            <Text>Review</Text>
-            <Text>Action</Text>
-            <Text>2019</Text>
-            <Text>193 minutes</Text>
+          <View style={styles.details}>
+            <MaterialIcons size={16} name="star" color="orange" />
+            <Text style={[styles.text, { color: "orange" }]}>9.5</Text>
+          </View>
+          <View style={styles.details}>
+            <MaterialIcons
+              size={16}
+              name="confirmation-number"
+              color={"#eee"}
+            />
+            <Text style={styles.text}>Action</Text>
+          </View>
+          <View style={styles.details}>
+            <MaterialIcons size={16} name="calendar-today" color={"#eee"} />
+            <Text style={styles.text}>2019</Text>
+          </View>
+          <View style={styles.details}>
+            <MaterialIcons size={16} name="access-time" color={"#eee"} />
+            <Text style={styles.text}>20 minutes</Text>
+          </View>
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    wrapper :{
-        flexDirection: 'row',
-        padding: 16,
-    }
-    ,ImageStyle:{
-        width: 100, 
-        height: 150,
-        borderRadius: 16,
-        marginRight: 16
-    },
-    titleStyle:{
-        fontSize: 16,
-        fontWeight: '400',
-        lineHeight: 24,
-        textAlign: 'left',
-        color: '#fff'
-    }
-})
+  wrapper: {
+    flexDirection: "row",
+    padding: 16,
+  },
+  ImageStyle: {
+    width: 100,
+    height: 150,
+    borderRadius: 16,
+    marginRight: 16,
+  },
+  titleStyle: {
+    fontSize: 16,
+    fontWeight: "400",
+    lineHeight: 24,
+    textAlign: "left",
+    color: "#fff",
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    textAlign: "left",
+  },
+  text: {
+    color: "#EEEEEE",
+    fontWeight: 400,
+    fontSize: 12,
+    lineHeight: 18,
+    marginLeft: 10,
+  },
+});
 
-export default Card
+export default Card;
